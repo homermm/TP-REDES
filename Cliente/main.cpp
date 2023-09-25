234d9e8ebcb943a68415b0787353b680dd1ba048
Homer <monsalvohomero@gmail.com>
2023-09-25 10:39:27 -0300

    bucle en el traductor añadido

--- Cliente/main.cpp
+++ Cliente/main.cpp
@@ -1,84 +1,81 @@
 #include <iostream>
+
 #include <cstring>
+
 #include <winsock2.h>
 
 using namespace std;
 
 class Client {
-public:
-    WSADATA WSAData;
-    SOCKET server;
-    SOCKADDR_IN addr;
-    char buffer[1024];
+  public: WSADATA WSAData;
+  SOCKET server;
+  SOCKADDR_IN addr;
+  char buffer[1024];
 
-    Client(const char *serverIP) {
-        cout << "Conectando al servidor..." << endl << endl;
-        WSAStartup(MAKEWORD(2, 0), &WSAData);
-        server = socket(AF_INET, SOCK_STREAM, 0);
-        addr.sin_addr.s_addr = inet_addr(serverIP); // Reemplaza con la dirección del servidor
-        addr.sin_family = AF_INET;
-        addr.sin_port = htons(5555);
-        connect(server, (SOCKADDR *)&addr, sizeof(addr));
-        cout << "Conectado al Servidor!" << endl;
+  Client(const char * serverIP) {
+    cout << "Conectando al servidor..." << endl << endl;
+    WSAStartup(MAKEWORD(2, 0), & WSAData);
+    server = socket(AF_INET, SOCK_STREAM, 0);
+    addr.sin_addr.s_addr = inet_addr(serverIP); // Reemplaza con la direcciï¿½n del servidor
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(5555);
+    connect(server, (SOCKADDR * ) & addr, sizeof(addr));
+    cout << "Conectado al Servidor!" << endl;
+  }
+
+  void Enviar(const string & mensaje) {
+    send(server, mensaje.c_str(), mensaje.size(), 0);
+  }
+
+  string Recibir() {
+    string mensaje;
+    int bytesRecibidos = recv(server, buffer, sizeof(buffer), 0);
+    if (bytesRecibidos <= 0) {
+      return "";
     }
+    mensaje.append(buffer, bytesRecibidos);
+    cout << "El servidor dice: " << mensaje << endl;
+    memset(buffer, 0, sizeof(buffer));
+    return mensaje;
+  }
 
-    void Enviar(const string &mensaje) {
-        send(server, mensaje.c_str(), mensaje.size(), 0);
-    }
+  void CerrarSocket() {
+    closesocket(server);
+    WSACleanup();
+    cout << "Socket cerrado." << endl << endl;
+  }
 
-    string Recibir() {
-        string mensaje;
-        int bytesRecibidos = recv(server, buffer, sizeof(buffer), 0);
-        if (bytesRecibidos <= 0) {
-            return "";
-        }
-        mensaje.append(buffer, bytesRecibidos);
-        cout << "El servidor dice: " << mensaje << endl;
-        memset(buffer, 0, sizeof(buffer));
-        return mensaje;
-    }
+  void MostrarMenu() {
+    system("cls"); // Borrar pantalla en Windows
+    cout << "\n\nMenu de Opciones" << endl;
+    cout << "1. Traducir" << endl;
+    cout << "2. Nueva traducciï¿½n" << endl;
+    cout << "3. Usuarios" << endl;
+    cout << "4. Ver registro de actividades" << endl;
+    cout << "5. Cerrar sesiï¿½n" << endl;
+    cout << "0. SALIR" << endl;
+  }
 
-    void CerrarSocket() {
-        closesocket(server);
-        WSACleanup();
-        cout << "Socket cerrado." << endl << endl;
-    }
-
-    void MostrarMenu()
-	{
-		//system("cls");	// Borrar pantalla en Windows
-		cout << "\n\nMenu de Opciones" << endl;
-		cout << "1. Traducir" << endl;
-		cout << "2. Nueva traducción" << endl;
-		cout << "3. Usuarios" << endl;
-		cout << "4. Ver registro de actividades" << endl;
-		cout << "5. Cerrar sesión" << endl;
-		cout << "0. SALIR" << endl;
-	}
-
-    ~Client() {
-        closesocket(server);
-        WSACleanup();
-    }
+  ~Client() {
+    closesocket(server);
+    WSACleanup();
+  }
 };
 
-
-
 int main() {
-    char serverIP[] = "127.0.0.1"; // Cambia la IP del servidor si es necesario
-    Client *Cliente = new Client(serverIP);
+  char serverIP[] = "127.0.0.1"; // Cambia la IP del servidor si es necesario
+  Client * Cliente = new Client(serverIP);
 
-    Cliente->MostrarMenu();
-    while(true){
-        //Cliente->MostrarMenu();
-        string opcion;
-        getline(cin, opcion);
+  Cliente -> MostrarMenu();
+  while (true) {
+    string opcion;
+    getline(cin, opcion);
 
-        Cliente->Enviar(opcion);
-        Cliente->Recibir();
-    }
-    delete Cliente;
-    return 0;
+    Cliente -> Enviar(opcion);
+    if (opcion == "/salir") Cliente -> MostrarMenu(); //Sale del bucle, se borra la consola y vuelve al menu
+    Cliente -> Recibir();
+
+  }
+  delete Cliente;
+  return 0;
 }
-
-
--- Servidor/main.cpp
+++ Servidor/main.cpp
@@ -1,178 +1,191 @@
 #include <iostream>
+
 #include <cstring>
+
 #include <winsock2.h>
+
 #include <fstream>
-#include <algorithm>  // Para transformar a minúsculas
+
+#include <algorithm>  // Para transformar a minï¿½sculas
 
 using namespace std;
 
 class Server {
-public:
-    WSADATA WSAData;
-    SOCKET server, client;
-    SOCKADDR_IN serverAddr, clientAddr;
-    char buffer[1024];
+  public: WSADATA WSAData;
+  SOCKET server,
+  client;
+  SOCKADDR_IN serverAddr,
+  clientAddr;
+  char buffer[1024];
 
-    Server() {
-        WSAStartup(MAKEWORD(2, 0), &WSAData);
-        server = socket(AF_INET, SOCK_STREAM, 0);
+  Server() {
+    WSAStartup(MAKEWORD(2, 0), & WSAData);
+    server = socket(AF_INET, SOCK_STREAM, 0);
 
-        serverAddr.sin_addr.s_addr = INADDR_ANY;
-        serverAddr.sin_family = AF_INET;
-        serverAddr.sin_port = htons(5555);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(5555);
 
-        bind(server, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
-        listen(server, 0);
+    bind(server, (SOCKADDR * ) & serverAddr, sizeof(serverAddr));
+    listen(server, 0);
 
-        cout << "Escuchando para conexiones entrantes en el puerto 5555." << endl;
-        int clientAddrSize = sizeof(clientAddr);
-        if ((client = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET) {
-            cout << "Cliente conectado!" << endl;
-        }
+    cout << "Escuchando para conexiones entrantes en el puerto 5555." << endl;
+    int clientAddrSize = sizeof(clientAddr);
+    if ((client = accept(server, (SOCKADDR * ) & clientAddr, & clientAddrSize)) != INVALID_SOCKET) {
+      cout << "Cliente conectado!" << endl;
+    }
+  }
+
+  //!FUNCIONES PRIMITIVAS
+  string Recibir() {
+    int bytesRecibidos = recv(client, buffer, sizeof(buffer), 0);
+    if (bytesRecibidos <= 0) {
+      CerrarSocket();
+      return "";
     }
 
-    //!FUNCIONES PRIMITIVAS
-    string Recibir() {
-        int bytesRecibidos = recv(client, buffer, sizeof(buffer), 0);
-        if (bytesRecibidos <= 0) {
-            CerrarSocket();
-            return "";
-        }
+    cout << "El cliente dice: " << buffer << endl;
+    string mensaje(buffer);
+    memset(buffer, 0, sizeof(buffer));
+    return mensaje;
+  }
 
-        cout << "El cliente dice: " << buffer << endl;
-        string mensaje(buffer);
-        memset(buffer, 0, sizeof(buffer));
-        return mensaje;
+  void Enviar(const string & mensaje) {
+    send(client, mensaje.c_str(), mensaje.size(), 0);
+    cout << "Mensaje enviado: " << mensaje << endl;
+  }
+
+  void CerrarSocket() {
+    closesocket(client);
+    cout << "Socket cerrado, cliente desconectado." << endl;
+  }
+
+  ~Server() {
+    closesocket(server);
+    WSACleanup();
+  }
+
+  //!FUNCIONES EXTRA
+  //!TRADUCTOR
+  void Traductor() {
+    Enviar("Inserte una palabra en ingles a traducir o escriba /salir para salir.");
+
+    while (true) {
+      string mensaje = Recibir();
+      if (mensaje.empty()) {
+        break; // El cliente se ha desconectado, sale del bucle interno
+      }
+
+      if (mensaje == "/salir") {
+        Enviar("Has vuelto al menu principal.");
+        break; // El usuario escribiÃ³ /salir, sale del bucle interno
+      }
+
+      // Buscar la traducciÃ³n en el archivo
+      string traduccion = BuscarTraduccionEnArchivo(mensaje);
+
+      // Enviar la traducciÃ³n al cliente
+      Enviar(traduccion);
+      //Enviar("Inserte otra palabra en inglÃ©s a traducir o escriba /salir para salir.");
+      //Se buguea si envio 2 mensajes seguidos asi q lo dejo comentado =)
     }
-
-    void Enviar(const string &mensaje) {
-        send(client, mensaje.c_str(), mensaje.size(), 0);
-        cout << "Mensaje enviado: " << mensaje << endl;
-    }
-
-    void CerrarSocket() {
-        closesocket(client);
-        cout << "Socket cerrado, cliente desconectado." << endl;
-    }
-
-    ~Server() {
-        closesocket(server);
-        WSACleanup();
-    }
-
-    //!FUNCIONES EXTRA
-    //!TRADUCTOR
-    void Traductor() {
-    Enviar("Inserte una palabra en ingles a traducir");
-    string palabraIngles = Recibir();
-
-    // Buscar la traducción en el archivo y enviarla
-    string traduccion = BuscarTraduccionEnArchivo(palabraIngles);
-
-    // Enviar la traducción al cliente
-    Enviar(traduccion);
-    }
-
-    string BuscarTraduccionEnArchivo(const string &palabraIngles) {
-    ifstream archivo("traducciones.txt");
-    if (archivo.is_open()) {
-        string palabraInglesMinusculas = ConvertirAMinusculas(palabraIngles); // Convertir la palabra a minúsculas
-        string linea;
-        while (getline(archivo, linea)) {
-            size_t pos = linea.find(":");
-            if (pos != string::npos) {
-                string palabra = linea.substr(0, pos);
-                string traduccion = linea.substr(pos + 1);
-                if (ConvertirAMinusculas(palabra) == palabraInglesMinusculas) { // Comparar en minúsculas
-                    archivo.close();
-                    return palabraIngles + " en ingles es " + traduccion + " en espanol";
-                }
-            }
-        }
-        archivo.close();
-    }
-    return "No fue posible encontrar la traducción para:" + palabraIngles;
-}
-    //!INSERTAR NUEVA TRADUCCION
-void InsertarNuevaTraduccion() {
-    Enviar("Ingrese nueva traducción (PalabraIngles:PalabraEspanol):");
+  }
+  //!INSERTAR NUEVA TRADUCCION
+  void InsertarNuevaTraduccion() {
+    Enviar("Ingrese nueva traducciï¿½n (PalabraIngles:PalabraEspanol):");
     string nuevaTraduccion = Recibir();
 
-    // Verificar el formato de la nueva traducción
+    // Verificar el formato de la nueva traducciï¿½n
     size_t separadorPos = nuevaTraduccion.find(':');
     if (separadorPos == std::string::npos) {
-        Enviar("No fue posible insertar la traducción. El formato de inserción debe ser PalabraIngles:PalabraEspanol");
-        return;
+      Enviar("No fue posible insertar la traducciï¿½n. El formato de inserciï¿½n debe ser PalabraIngles:PalabraEspanol");
+      return;
     }
 
-    // Obtener la palabra en inglés y la traducción
+    // Obtener la palabra en inglï¿½s y la traducciï¿½n
     string palabraIngles = nuevaTraduccion.substr(0, separadorPos);
     string traduccion = nuevaTraduccion.substr(separadorPos + 1);
 
-    // Convertir las palabras a minúsculas
+    // Convertir las palabras a minï¿½sculas
     palabraIngles = ConvertirAMinusculas(palabraIngles);
     traduccion = ConvertirAMinusculas(traduccion);
 
     // Verificar si la palabra ya existe en las traducciones
     ifstream archivo("traducciones.txt");
     if (archivo.is_open()) {
-        string linea;
-        while (getline(archivo, linea)) {
-            size_t pos = linea.find(":");
-            if (pos != string::npos) {
-                string palabraExistente = linea.substr(0, pos);
-                string traduccionExistente = linea.substr(pos + 1);
-                if (ConvertirAMinusculas(palabraExistente) == palabraIngles) {
-                    archivo.close();
-                    Enviar("Ya existe una traducción para " + palabraIngles + ": " + traduccionExistente);
-                    return;
-                }
-            }
+      string linea;
+      while (getline(archivo, linea)) {
+        size_t pos = linea.find(":");
+        if (pos != string::npos) {
+          string palabraExistente = linea.substr(0, pos);
+          string traduccionExistente = linea.substr(pos + 1);
+          if (ConvertirAMinusculas(palabraExistente) == palabraIngles) {
+            archivo.close();
+            Enviar("Ya existe una traducciï¿½n para " + palabraIngles + ": " + traduccionExistente);
+            return;
+          }
         }
-        archivo.close();
+      }
+      archivo.close();
     }
 
-    // Agregar la nueva traducción al archivo en una línea separada con salto de línea
+    // Agregar la nueva traducciï¿½n al archivo en una lï¿½nea separada con salto de lï¿½nea
     ofstream archivoSalida("traducciones.txt", std::ios::app); // Abre el archivo en modo de apertura al final
     if (!archivoSalida.is_open()) {
-        Enviar("Error al abrir el archivo de diccionario para inserción.");
-        return;
+      Enviar("Error al abrir el archivo de diccionario para inserciï¿½n.");
+      return;
     }
 
     archivoSalida << palabraIngles << ":" << traduccion << std::endl;
     archivoSalida.close();
 
-    Enviar("Nueva traducción insertada correctamente");
-}
+    Enviar("Nueva traducciï¿½n insertada correctamente");
+  }
 
-//!FUNCIONES AUXILIARES
-string ConvertirAMinusculas(const string &cadena) {
+  //!FUNCIONES AUXILIARES
+  string ConvertirAMinusculas(const string & cadena) {
     string cadenaMinusculas = cadena;
     transform(cadenaMinusculas.begin(), cadenaMinusculas.end(), cadenaMinusculas.begin(), ::tolower);
     return cadenaMinusculas;
-}
+  }
 
+  string BuscarTraduccionEnArchivo(const string & palabraIngles) {
+    ifstream archivo("traducciones.txt");
+    if (archivo.is_open()) {
+      string palabraInglesMinusculas = ConvertirAMinusculas(palabraIngles); // Convertir la palabra a minï¿½sculas
+      string linea;
+      while (getline(archivo, linea)) {
+        size_t pos = linea.find(":");
+        if (pos != string::npos) {
+          string palabra = linea.substr(0, pos);
+          string traduccion = linea.substr(pos + 1);
+          if (ConvertirAMinusculas(palabra) == palabraInglesMinusculas) { // Comparar en minï¿½sculas
+            archivo.close();
+            return palabraIngles + " en ingles es " + traduccion + " en espanol";
+          }
+        }
+      }
+      archivo.close();
+    }
+    return "No fue posible encontrar la traducciï¿½n para:" + palabraIngles;
+  }
 };
 
-
 int main() {
-    Server *Servidor = new Server();
-    while (true) {
-        string opcion = Servidor->Recibir();
+  Server * Servidor = new Server();
+  while (true) {
+    string opcion = Servidor -> Recibir();
 
-        if (opcion.empty()) break; // El cliente se ha desconectado, sale del bucle
+    if (opcion.empty()) break; // El cliente se ha desconectado, sale del bucle
 
-        else if (opcion == "1") Servidor->Traductor();
-        else if (opcion == "2") Servidor->InsertarNuevaTraduccion(); // funcion nueva trad
-        else if (opcion == "3") Servidor->Enviar("Funcion todavia no implementada"); // funcion usuarios
-        else if (opcion == "4") Servidor->Enviar("Funcion todavia no implementada"); // funcion registro act
-        else if (opcion == "5") Servidor->Enviar("Funcion todavia no implementada"); // funcion cerrar sesion
-        else if (opcion == "0") Servidor->Enviar("Funcion todavia no implementada"); // funcion salir
-        else Servidor->Enviar("Inserte una opcion (Disponible 1 y 2)");
-    }
-    delete Servidor;
-    return 0;
+    else if (opcion == "1") Servidor -> Traductor();
+    else if (opcion == "2") Servidor -> InsertarNuevaTraduccion(); // funcion nueva trad
+    else if (opcion == "3") Servidor -> Enviar("Funcion todavia no implementada"); // funcion usuarios
+    else if (opcion == "4") Servidor -> Enviar("Funcion todavia no implementada"); // funcion registro act
+    else if (opcion == "5") Servidor -> Enviar("Funcion todavia no implementada"); // funcion cerrar sesion
+    else if (opcion == "0") Servidor -> Enviar("Funcion todavia no implementada"); // funcion salir
+    else Servidor -> Enviar("Inserte una opcion (Disponible 1 y 2)");
+  }
+  delete Servidor;
+  return 0;
 }
-
-
-
